ff1d378659511b720b70ad4dd0384ee80290d454
ashwini <ashwini@kkvesper.jp>
2017-02-08 13:00:35 +0900

    Adding line graph

--- components/app.jsx
+++ components/app.jsx
@@ -6,15 +6,29 @@ import d3Chart from '../graphs/d3Chart.js';
 const data = [
   { id: '1', x: 2010, y: 50 },
   { id: '2', x: 2011, y: 60 },
-  { id: '1', x: 2016, y: 80 },
-  { id: '2', x: 2015, y: 60 },
-  { id: '1', x: 2018, y: 90 },
-  { id: '2', x: 2020, y: 20 },
-  { id: '1', x: 2017, y: 50 },
-  { id: '2', x: 2013, y: 40 },
-  { id: '1', x: 2016, y: 80 },
-  { id: '2', x: 2010, y: 30 }
+  { id: '3', x: 2016, y: 80 },
+  { id: '4', x: 2015, y: 60 },
+  { id: '5', x: 2018, y: 90 },
+  { id: '6', x: 2020, y: 20 },
+  { id: '7', x: 2017, y: 50 },
+  { id: '8', x: 2013, y: 40 },
+  { id: '9', x: 2016, y: 80 },
+  { id: '10', x: 2010, y: 30 }
 ]
+
+const lineData = [
+  { id: '1', x: 2010, y: 5 },
+  { id: '2', x: 2011, y: 10 },
+  { id: '3', x: 2012, y: 6 },
+  { id: '4', x: 2013, y: 8 },
+  { id: '5', x: 2014, y: 0 },
+  { id: '6', x: 2015, y: 3 },
+  { id: '7', x: 2016, y: 6 },
+  { id: '8', x: 2017, y: 8 },
+  { id: '9', x: 2018, y: 5 },
+  { id: '10', x: 2019, y: 8 }
+]
+
 
 class App extends React.Component {
 
@@ -34,7 +48,8 @@ class App extends React.Component {
   getChartState() {
     return {
       data: data,
-      domain: {x: [new Date('Jan 01, 2010'), new Date('Jan 01, 2020')], y: [100, 0]}
+      domain: {x: [new Date('Jan 01, 2010'), new Date('Jan 01, 2020')], y: [100, 0]},
+      lineData: lineData
     };
   }
 
@@ -44,9 +59,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="react-graphs-app">
-        <div className="chart"></div>
-      </div>
+      <div className="react-graphs-app" />
     );
   }
 }
--- graphs/d3Chart.js
+++ graphs/d3Chart.js
@@ -15,17 +15,27 @@ d3Chart.create = function(el, props, state) {
 
   //Create the Scale we will use for the Axis
   var xAxisScale = d3.scaleUtc()
-                          .domain(state.domain.x)
-                          .range([0, props.width]);
+                      .domain(state.domain.x)
+                      .range([0, props.width]);
 
   //Create the Scale we will use for the Axis
   var yAxisScale = d3.scaleLinear()
-                          .domain(state.domain.y)
-                          .range([0, props.height]);
+                      .domain(state.domain.y)
+                      .range([0, props.height]);
+
+  var y1AxisScale = d3.scaleLinear()
+                      .domain([10, 0])
+                      .range([0, props.height]);
 
   //Create the Axis
   var xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.timeFormat("%Y"));
   var yAxis = d3.axisLeft(yAxisScale);
+  var y1Axis = d3.axisRight(y1AxisScale);
+
+  svg.append("g")
+      .attr('class', 'y1-axis')
+      .attr("transform", "translate("+props.width+", 0)")
+      .call(y1Axis);
 
   svg.append("g")
       .attr('class', 'y-axis')
@@ -39,6 +49,19 @@ d3Chart.create = function(el, props, state) {
   svg.append('g')
       .attr('class', 'd3-points');
 
+  var clip = svg.append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", props.width)
+      .attr("height", props.height);
+
+  svg.append('g')
+      .attr('class', 'd3-line-graph')
+      .attr("clip-path", "url(#clip)")
+      .style("stroke-width",2.5);
+
   this.update(el, props, state);
 };
 
@@ -46,10 +69,62 @@ d3Chart.update = function(el, props, state) {
   // Re-compute the scales, and render the data points
   var scales = this._scales(el, state.domain);
   this._drawPoints(el, scales, state.data, props);
+
+  var linedata = [{ values: state.lineData.map(function(d) { return { date: d.x, value: parseFloat(d.y,10) }; }) }];
+
+  var lineScales = this._lineScales(el, linedata);
+
+  var line = d3.line()
+      .x(function(d) { return lineScales.x(d.date); })
+      .y(function(d) { return lineScales.y(d.value); });
+
+  this._drawLine(el, line, linedata);
 };
 
 d3Chart.destroy = function(el) {
 
+};
+
+d3Chart._drawLine = function(el, line, lineData) {
+  var lineGraph = d3.select(el).selectAll(".d3-line-graph");
+
+  var lines = lineGraph.selectAll('.d3-line')
+                        .data(lineData)
+                        .enter()
+                        .append('path')
+                        .attr("class", "d3-line")
+                        .attr("d", function(d) { return line(d.values[0]); })
+                        .transition()
+                        .duration(2000)
+                        .attrTween('d',function (d) {
+                          var interpolate = d3.scaleQuantile()
+                                                    .domain([0,1])
+                                                    .range(d3.range(1, d.values.length+1));
+                          return function(t) {
+                            return line(d.values.slice(0, interpolate(t)));
+                          };
+                        });
+};
+
+d3Chart._lineScales = function(el, linedata) {
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  var x = d3.scaleUtc()
+    .domain([
+      d3.min(linedata, function(c) { return d3.min(c.values, function(v) { return v.date; }); }),
+      d3.max(linedata, function(c) { return d3.max(c.values, function(v) { return v.date; }); })
+    ])
+    .range([0, width]);
+
+  var y = d3.scaleLinear()
+    .domain([
+      d3.min(linedata, function(c) { return d3.min(c.values, function(v) { return v.value; }); }),
+      d3.max(linedata, function(c) { return d3.max(c.values, function(v) { return v.value; }); })
+    ])
+    .range([height, 0]);
+
+  return {x: x, y: y};
 };
 
 d3Chart._scales = function(el, domain) {
@@ -60,7 +135,7 @@ d3Chart._scales = function(el, domain) {
   var width = el.offsetWidth;
   var height = el.offsetHeight;
 
-  var x = d3.scaleLinear()
+  var x = d3.scaleUtc()
     .range([0, width])
     .domain(domain.x);
 
